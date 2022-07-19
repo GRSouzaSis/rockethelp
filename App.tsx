9fa296763bb3f747939935b49a1287e64abc9247
Givaldo Souza <givaribeiro23@gmail.com>
2022-07-19 09:08:40 -0300

    feat: screen Regiter, Details, Header

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {NativeBaseProvider, StatusBar } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { THEME } from './src/styles/theme'
-import { Home } from './src/screens/Home';
+import { Register } from './src/screens/Register';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontLoaded ? <Home /> : <Loading /> }
+      {fontLoaded ? <Register /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
